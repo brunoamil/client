244d74075453e8ae0a70d35dbc337f9a4bf9bd42
Bruno Lima <bruno-amil@hotmail.com>
2020-11-08 17:34:10 -0300

    alterando o style e stories da logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -14,27 +14,25 @@ const wrapperModifiers = {
     height: 5.9rem;
   `,
 
-  hidenOnMobile: () => css`
+  hideOnMobile: () => css`
     ${media.lessThan('medium')`
-    width: 5.8rem;
-    height: 4.5rem;
-
-    svg {
-     height: 4.5rem;
-     pointer-events: none;
-    }
-    .text {
-      display: none;
-    }
-  `}
+      width: 5.8rem;
+      height: 4.5rem;
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size, hidenOnMobile }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
-
-    ${!!size && wrapperModifiers[size]};
-    ${!!hidenOnMobile && wrapperModifiers.hidenOnMobile};
+    ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,16 +1,22 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-import 'jest-styled-components'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
+  it('should render the logo with id passed', () => {
+    const { container } = renderWithTheme(<Logo id="myId" />)
+
+    expect(container.querySelector('#paint_linear_myId')).toBeInTheDocument()
+  })
+
   it('should render a white label by default', () => {
     renderWithTheme(<Logo />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#FAFAFA'
     })
   })
+
   it('should render a black label when color is passed', () => {
     renderWithTheme(<Logo color="black" />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
@@ -33,7 +39,7 @@ describe('<Logo />', () => {
   })
 
   it('should render a bigger logo without text if hideOnMobile', () => {
-    renderWithTheme(<Logo hidenOnMobile />)
+    renderWithTheme(<Logo hideOnMobile />)
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
       'width',
       '5.8rem',
