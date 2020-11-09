6a8277290e104913bae34cef3b8793be4415483b
Bruno Lima <bruno-amil@hotmail.com>
2020-11-09 15:09:24 -0300

    ajustando o hover do button

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -50,4 +50,18 @@ describe('<Button />', () => {
     expect(screen.getByText(/buy now/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
   })
+
+  it('should render button as a link', () => {
+    const { debug, container } = renderWithTheme(
+      <Button as="a" href="/link">
+        Buy Now
+      </Button>
+    )
+    debug(container)
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
+  })
 })
