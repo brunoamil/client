a2e5777eec11e8d113eb1f6f1659321b63cf1d79
Bruno Lima <bruno-amil@hotmail.com>
2020-11-09 22:47:19 -0300

    criando o banner e testando

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Banner, { BannerProps } from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner,
+  args: {
+    img: 'https://images4.alphacoders.com/104/1049586.jpg',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: Story<BannerProps> = (args) => <Banner {...args} />
---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+const props = {
+  img: 'http://www.ceuma.edu.br',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
