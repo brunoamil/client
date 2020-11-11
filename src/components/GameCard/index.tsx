80ed2e4288e0e12aa83e473b75c78a4a11f3b4f0
Bruno Lima <bruno-amil@hotmail.com>
2020-11-10 23:55:20 -0300

    criando component gamecard

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,32 @@
+import { Favorite, AddShoppingCart } from '@styled-icons/material-outlined'
+import Button from 'components/Button'
+import * as S from './styles'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+}
+const GameCard = ({ title, developer, img, price }: GameCardProps) => (
+  <S.Wrapper>
+    <S.ImageBox>
+      <img src={img} alt={title} />
+    </S.ImageBox>
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton role="button">
+        <Favorite aria-label="Add to wishilist" />
+      </S.FavButton>
+      <S.BuyBox>
+        <S.Price>{price}</S.Price>
+        {/* <Button icon={AddShoppingCart} size={small} /> */}
+      </S.BuyBox>
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default GameCard
---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  args: {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img:
+      'https://cdn.carsp.com.br/upload/imgcache/9a1b24749f34c5c8b25e47d91aafb815.jpg',
+    price: 'R$ 235,00'
+  }
+} as Meta
+
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
