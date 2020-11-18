d251b1fd208b38f7242e34ad0c30b9cfe284cfc8
Bruno Lima <bruno-amil@hotmail.com>
2020-11-18 09:15:47 -0300

    criando o bannerslider

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,21 @@
+import Banner, { BannerProps } from 'components/Banner'
+import Slider from 'components/Slider'
+import * as S from './styles'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings = {}
+
+const BannerSlider = ({ items }: BannerSliderProps) => (
+  <S.Wrapper>
+    <Slider settings={settings}>
+      {items.map((item) => (
+        <Banner key={item.title} {...item} />
+      ))}
+    </Slider>
+  </S.Wrapper>
+)
+
+export default BannerSlider
