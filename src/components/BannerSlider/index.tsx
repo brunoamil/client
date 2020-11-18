902efeb1bbf66f7af98d7d80703b91c0819899a1
Bruno Lima <bruno-amil@hotmail.com>
2020-11-18 19:24:26 -0300

    finalizando o estilo do bannerslider

--- src/components/BannerSlider/index.tsx
+++ src/components/BannerSlider/index.tsx
@@ -1,12 +1,27 @@
 import Banner, { BannerProps } from 'components/Banner'
-import Slider from 'components/Slider'
+import Slider, { SliderSettings } from 'components/Slider'
 import * as S from './styles'
 
 export type BannerSliderProps = {
   items: BannerProps[]
 }
 
-const settings = {}
+const settings: SliderSettings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+  ]
+}
 
 const BannerSlider = ({ items }: BannerSliderProps) => (
   <S.Wrapper>
--- src/components/BannerSlider/stories.tsx
+++ src/components/BannerSlider/stories.tsx
@@ -1,9 +1,45 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import BannerSlider from '.'
+import BannerSlider, { BannerSliderProps } from '.'
 
+const items = [
+  {
+    img: 'https://images4.alphacoders.com/104/1049586.jpg',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://images4.alphacoders.com/104/1049586.jpg',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://images4.alphacoders.com/104/1049586.jpg',
+    title: 'Defy death 3',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
 export default {
   title: 'BannerSlider',
-  component: BannerSlider
+  component: BannerSlider,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <BannerSlider />
+export const Default: Story<BannerSliderProps> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <BannerSlider {...args} />
+  </div>
+)
